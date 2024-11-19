84baca26d9766aeba5eab146b1b4bf57be5bc970
aoringo1011 <cguh1011@mail4.doshisha.ac.jp>
2024-11-19 22:59:16 +0900

    Update addition.c with sum of three

--- addition.c
+++ addition.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
+    int num1, num2, num3, sum;
 
     // ユーザーに入力を促す
     printf("1つ目の数字を入力してください: ");
@@ -10,8 +10,11 @@ int main() {
     printf("2つ目の数字を入力してください: ");
     scanf("%d", &num2);
 
+    printf("3つ目の数字を入力してください: ");
+    scanf("%d", &num3);
+
     // 足し算を計算
-    sum = num1 + num2;
+    sum = num1 + num2 + num3;
 
     // 結果を表示
     printf("合計は %d です。\n", sum);
